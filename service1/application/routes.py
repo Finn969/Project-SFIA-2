9573fbe3f909744b1fac53ae86b5c8da55ea8578
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-03-26 11:12:14 +0000

    html

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -3,6 +3,13 @@ from flask import render_template, request
 import requests
 import random
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def home():
     return render_template('home.html')
+
+@app.route('/horoscope', methods=['GET'])
+def horoscope():
+    response = requests.get('http://service_4:5003/')
+    print(response)
+    sentence = response.text
+    return render_template('output.html',sentence=sentence)
