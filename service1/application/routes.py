798e3740f8b29893a154b2aedce535413d629b09
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-03-27 12:05:16 +0000

    testing routes

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,19 +1,24 @@
 from application import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import requests
-import random
+import json
 
 @app.route('/', methods=['POST','GET'])
 def home():
     if request.method == 'POST':
         details = request.form
-        requests.put('http://service_2:5001', data = [details['dob'],details['gender']])
-        
-   return render_template('home.html')
+        DOB = details["dob"]
+        DOB = json.dumps(DOB)
+        print (DOB)
+
+        requests.post('http://localhost:5001', json=DOB)
+        return redirect(url_for('horoscope'))
+    return render_template('home.html')
 
 @app.route('/horoscope', methods=['POST','GET'])
 def horoscope():
     response = requests.get('http://service_4:5003/')
     print(response)
     sentence = response.text
+    print (sentence)
     return render_template('output.html',sentence=sentence)
--- service2/application/routes.py
+++ service2/application/routes.py
@@ -1,4 +1,15 @@
-#Placeholder
+from application import app
+from flask import render_template, request, redirect, url_for
+import requests
 
 #This service will generate astrological info from the dob
 
+@app.route('/', methods=['GET','POST'])
+def astroinfo():
+    starsign = ''
+    birthday = requests. 
+
+    # code to determine starsign
+    # test output
+    starsign = 'Libra'
+    return starsign
