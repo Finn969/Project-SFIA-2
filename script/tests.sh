27362cc02b226c6f70d64c28a2e8ae4f66c95340
Finn969 <faustinmacrae@gmail.com>
2020-04-14 06:21:47 +0100

    tried to open permissions to tests

--- script/tests.sh
+++ script/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ~/.bashrc
+sudo chmod -R a+rw ./tests/
 python3 -m pytest ./tests/testing.py
 python3 -m coverage run -m --source=. pytest ./tests/testing.py
 python3 -m coverage report -m ./tests/testing.py
