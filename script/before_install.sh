450ad9d3f5dcc1a31314ef94aa27276b69209cfb
Finn969 <faustinmacrae@gmail.com>
2020-04-14 03:57:25 +0100

    preparing for pipeline

--- script/before_install.sh
+++ script/before_install.sh
@@ -2,6 +2,17 @@
 apt update -y
 apt install python3 -y
 apt install python3-pip -y
-apt install python3-venv -y
+
 apt-get install libmysqlclient-dev -y
-python3 -m venv venv
+
+
+mkdir -p ~/.local/bin
+touch ~/.bashrc
+echo 'PATH=$PATH:~/.local/bin' > ~/.bashrc
+
+sudo chown -R $(whoami):$(whoami) ~/*
+source ~/.bashrc
+
+pip install --user ansible
+
+ansible --version
