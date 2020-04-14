f6c67487dd1ddc82448e30a091f62d64e5a727cd
Finn969 <faustinmacrae@gmail.com>
2020-04-14 04:16:35 +0100

    made before_install commands sudo

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-apt update -y
-apt install python3 -y
-apt install python3-pip -y
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 
-apt-get install libmysqlclient-dev -y
+sudo apt-get install libmysqlclient-dev -y
 
 
-mkdir -p ~/.local/bin
-touch ~/.bashrc
+sudo mkdir -p ~/.local/bin
+sudo touch ~/.bashrc
 echo 'PATH=$PATH:~/.local/bin' > ~/.bashrc
 
 sudo chown -R $(whoami):$(whoami) ~/*
